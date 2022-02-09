a3c75be882c7c29886f70ae6c2c41e35320559b1
dev-Pedrod <pedroolivxira@gmail.com>
2022-02-09 15:48:00 -0300

    feat(Exceptions): Object not found exception

---
+++ src/main/java/com/devpedrod/registrationlogin/service/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,7 @@
+package com.devpedrod.registrationlogin.service.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+    public ObjectNotFoundException(String msg) {
+        super(msg);
+    }
+}
